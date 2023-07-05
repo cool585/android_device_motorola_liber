1b4c08bca09bb632197608a60a1ced0d71ce2da4
DhineshCool <dhineshcool585@gmail.com>
2023-07-05 23:17:16 +0530

    liber: Bringup superior os

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_liber.mk
+    $(LOCAL_DIR)/superior_liber.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_liber-eng \
-    lineage_liber-user \
-    lineage_liber-userdebug
+    superior_liber-eng \
+    superior_liber-user \
+    superior_liber-userdebug
--- superior_liber.mk
+++ superior_liber.mk
@@ -21,11 +21,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from liber device
 $(call inherit-product, device/motorola/liber/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common superior stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official SuperiorOS
+SUPERIOR_OFFICIAL := true
+DEVICE_MAINTAINERS="Dhinesh"
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_liber
+PRODUCT_NAME := superior_liber
 PRODUCT_DEVICE := liber
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Motorola One Fusion+
